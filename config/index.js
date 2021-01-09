ea3642b23c66b08566005dc7ec97e0a61e18827e
alonzo <alonzoalden@gmail.com>
2021-01-08 16:33:19 -0800

    nodemailer updates

--- config/index.js
+++ config/index.js
@@ -3,6 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 var transporter = nodemailer.createTransport({
   service: 'gmail',
+  // host: 'smtp.gmail.com',
+  // port: 587,
+  // secure: false,
+  // requireTLS: true,
   auth: {
     user: process.env.email,
     pass: process.env.password
--- routes/api/email.js
+++ routes/api/email.js
@@ -6,27 +6,27 @@ dotenv.config();
 router.post('/review', (req, res) => {
 	try {
 		const mailOptions = {
-			from: req.body.email, // sender address
-			to: process.env.email, // list of receivers
+			from: `WGFS Web Services <${process.env.email}>`, // sender address
+			to: process.env.emailto, // list of receivers
 			subject: 'WGFS Review Submitted', // Subject line
 			html: `
-		<p>You have a new review.</p>
-		<h3>Review Details</h3>
-		<ul>
-		  <li>Name: ${req.body.name}</li>
-		  <li>Email: ${req.body.email}</li>
-		  <li>Rating: ${req.body.rating}</li>
-		  <li>Message: ${req.body.message}</li>
-		</ul>
-		`
+				<p>You have a new review.</p>
+				<h3>Review Details</h3>
+				<ul>
+				<li>Name: ${req.body.name}</li>
+				<li>Email: ${req.body.email}</li>
+				<li>Rating: ${req.body.rating} out of 5</li>
+				<li>Message: ${req.body.message}</li>
+				</ul>
+			`
 		};
-
 		transporter.sendMail(mailOptions, function (err, info) {
 			if (err) {
 				res.status(500).send({
 					success: false,
-					message: 'Something went wrong. Try again later 2'
+					message: 'Gmail rejected you. Please check.'
 				});
+				console.log(err)
 			} else {
 				res.send({
 					success: true,
