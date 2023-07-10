ebbfd26cef9c6e1878ffd3b27c3707123bdf4e1c
Alonzo Alden <alonzoalden@gmail.com>
2023-07-09 21:14:11 -0700

    error 413

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 
@@ -43,7 +43,7 @@ const corsOptions ={
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000 }));
 
 app.use(require('method-override')());
 app.use(express.static(__dirname + '/dist'));
